035f74d64bb2ce769d36e558bfbf501173439622
abhinaya-sinha <asinha15@illinois.edu>
2022-07-14 17:14:29 -0500

    minor change

--- create_histograms.py
+++ create_histograms.py
@@ -115,7 +115,7 @@ for Msim in Msimuniq:
                 plt.legend()
                 title = 'M' + str(int(Msim/100)) + 'G' + str(int(Gsim*100/Msim) if Gsim*100/Msim >=100 else ('0' + str(int(Gsim*100/Msim)) if Gsim*100/Msim >= 10 else '00' + str(int(Gsim*100/Msim)))) + '(s)' + ' M' + str(int(Mtarget/100)) + 'G' + str(int(Gtarget*100/Mtarget) if Gtarget*100/Mtarget >= 100 else ('0' + str(int(Gtarget*100/Mtarget)) if Gtarget*100/Mtarget >= 10 else '00' + str(int(Gtarget*100/Mtarget)))) + '(r)'
                 plt.title(title)
-                plt.savefig('/projects/bbhj/asinha15/VLQ-NN-Reweighting/main/histograms/' + title + '.png')
+                plt.savefig('/projects/bbhj/asinha15/VLQ-NN-Reweighting/main/histograms/' + title.replace(' ', '') + '.png')
                 del idx
             del Mtargetidx
         del Gsimidx
