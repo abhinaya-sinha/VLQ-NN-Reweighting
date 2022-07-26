acfc5c09a8ffeba4ceb0266ee5d8b6d6cdcc875d
abhinaya-sinha <asinha15@illinois.edu>
2022-07-26 17:49:11 -0500

    Update run.py

--- DNN/run.py
+++ DNN/run.py
@@ -19,29 +19,23 @@ else:
 features = ['Msim',
 'Gsim',
 'pz_in1',
-'pid_in1', 
 'pz_in2',
-'pid_in2',
 'px_out1',
 'py_out1',
 'pz_out1',
 'e_out1',
-'pid_out1',
 'px_out2',
 'py_out2',
 'pz_out2',
 'e_out2',
-'pid_out2',
 'px_out3',
 'py_out3',
 'pz_out3',
 'e_out3',
-'pid_out3',
 'px_out4',
 'py_out4',
 'pz_out4',
 'e_out4',
-'pid_out4',
 'Mtarget',
 'Gtarget',
 'mode-W', 
@@ -74,14 +68,14 @@ VLQData = CSVData(batch_size=2048, features_name=features, labels_name=label, fe
 test_data = CSVData(batch_size=1024, features_name=features, labels_name=label, features_to_rescale= features_to_rescale, file_names=['/projects/bbhj/asinha15/test_' + str(i) + '.csv' for i in range(0,10)])
 val_data = CSVData(batch_size=2048, features_name=features, labels_name=label, features_to_rescale= features_to_rescale, file_names=['/projects/bbhj/asinha15/train_'+str(i)+'.csv' for i in range(8,10)])
 
-net = DNN(Layers=[31, 32, 64, 32, 32, 16, 8, 4], device=device).Model
+net = DNN(Layers=[25, 32, 64, 32, 32, 16, 8, 4], device=device).Model
 optimizer = optim.Adam(net.parameters(), lr=1e-3)
 epochs=300
 
 losses, test_losses, val_losses, accuracies = train_model.train(train_data=VLQData, test_data = test_data, val_data = val_data, net = net, optimizer=optimizer, epochs=epochs, device=device)
 
 model_scripted = torch.jit.script(net)
-model_scripted.save('/projects/bbhj/asinha15/VLQ-NN-Reweighting/main/trained_models/[31, 32, 64, 32, 32, 16, 8, 4].pt')
+model_scripted.save('/projects/bbhj/asinha15/VLQ-NN-Reweighting/main/trained_models/[25, 32, 64, 32, 32, 16, 8, 4].pt')
 
 plt.plot(np.linspace(0,len(losses), len(losses)), losses, label = 'train loss')
 plt.yscale('log')
@@ -90,12 +84,12 @@ plt.yscale('log')
 plt.plot(np.linspace(0, len(losses), len(losses)), val_losses, label = 'validation loss')
 plt.yscale('log')
 plt.legend()
-plt.savefig('/projects/bbhj/asinha15/VLQ-NN-Reweighting/main/plots/LossFunctionPlots/[31, 32, 64, 32, 32, 16, 8, 4].png')
+plt.savefig('/projects/bbhj/asinha15/VLQ-NN-Reweighting/main/plots/LossFunctionPlots/[25, 32, 64, 32, 32, 16, 8, 4].png')
 plt.show()
 plt.close()
 
 plt.plot(np.linspace(0,len(losses),len(losses)), accuracies)
 plt.title('validation accuracy')
-plt.savefig('/projects/bbhj/asinha15/VLQ-NN-Reweighting/main/plots/RAE/[31, 32, 64, 64, 32, 16, 8, 4].png')
+plt.savefig('/projects/bbhj/asinha15/VLQ-NN-Reweighting/main/plots/RAE/[25, 32, 64, 64, 32, 16, 8, 4].png')
 plt.show()
 plt.close()
